86d91054e3ac8bdff381c96fcb608b0c5214ee96
sheason <811627877@qq.com>
2024-05-08 16:10:13 +0000

    feat: 使用 nodejs 作为接口生成脚本

---
+++ TypeSpec/script/gen-web.mjs
@@ -0,0 +1,26 @@
+//@ts-check
+import { exec } from "child_process";
+import { findNamespaces } from "./gen-common.mjs";
+
+function main() {
+  const namespaces = findNamespaces();
+  console.log("正在生成 Web 请求库", namespaces);
+  namespaces.forEach(
+    ns => {
+      const child = exec(
+        `npx nswag openapi2tsclient ` +
+        `/input:"./tsp-output/@typespec/openapi3/openapi.${ns}.yaml" ` +
+        `/output:"../Applications/Web/api-client/${ns}.client.ts" ` +
+        `/ClassName:"${ns}Client" ` +
+        `/TypeStyle:Interface`,
+        (err) => {
+          if (err) {
+            throw err;
+          }
+        }
+      )
+    },
+  );
+}
+
+main();
