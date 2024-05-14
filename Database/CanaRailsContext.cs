5c210b23dd8e4393e99294fcfefc4af74f46c758
sheason <811627877@qq.com>
2024-05-14 23:54:26 +0800

    feat: 使用环境变量连接 Postgres 数据库

--- Database/CanaRailsContext.cs
+++ Database/CanaRailsContext.cs
@@ -4,24 +4,34 @@ namespace CanaRails.Database;
 
 public class CanaRailsContext : DbContext
 {
-  public DbSet<Entities.App> Apps { get; set; }
-  public DbSet<Entities.AppMatcher> AppMatchers { get; set; }
-  public DbSet<Entities.Entry> Entries { get; set; }
-  public DbSet<Entities.EntryMatcher> EntryMatchers { get; set; }
-  public DbSet<Entities.Image> Images { get; set; }
-  public DbSet<Entities.Container> Containers { get; set; }
+    public DbSet<Entities.App> Apps { get; set; }
+    public DbSet<Entities.AppMatcher> AppMatchers { get; set; }
+    public DbSet<Entities.Entry> Entries { get; set; }
+    public DbSet<Entities.EntryMatcher> EntryMatchers { get; set; }
+    public DbSet<Entities.Image> Images { get; set; }
+    public DbSet<Entities.Container> Containers { get; set; }
 
-  public string DbPath { get; }
+    public string DbPath { get; }
 
-  public CanaRailsContext()
-  {
-    var folder = Environment.SpecialFolder.LocalApplicationData;
-    var path = Environment.GetFolderPath(folder);
-    DbPath = Path.Join(path, "cana-rails.db");
-  }
+    public CanaRailsContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = Path.Join(path, "cana-rails.db");
+    }
 
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-  {
-    optionsBuilder.UseSqlite($"Data Source={DbPath}");
-  }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var host = Environment.GetEnvironmentVariable("Host");
+        var dbname = Environment.GetEnvironmentVariable("Database");
+        var username = Environment.GetEnvironmentVariable("postgres");
+        var password = Environment.GetEnvironmentVariable("password");
+
+        optionsBuilder.UseNpgsql(
+            $"Host={host};" +
+            $"Database={dbname};" +
+            $"Username={username};" +
+            $"Password={password}"
+        );
+    }
 }
--- Manage/Program.cs
+++ Manage/Program.cs
@@ -9,72 +9,58 @@ using CanaRails.Controllers.Image;
 using CanaRails.Controllers.Entry;
 using CanaRails.Controllers.AppMatcher;
 using CanaRails.Adapters.IAdapter;
-using Microsoft.EntityFrameworkCore;
 
 namespace CanaRails.Manage;
 
 public class Program
 {
-  public static WebApplication CreateApplication()
-  {
-    var builder = WebApplication.CreateBuilder();
-
-    builder.Services.AddEndpointsApiExplorer();
-    
-    builder.Services.AddDbContext<CanaRailsContext>();
-
-    // Add Container adapter
-    builder.Services.AddSingleton<IAdapter, DockerAdapter>();
-
-    // Add Services
-    builder.Services.AddSingleton<AppService>();
-    builder.Services.AddSingleton<AppMatcherService>();
-    builder.Services.AddSingleton<DockerService>();
-    builder.Services.AddSingleton<ImageService>();
-    builder.Services.AddSingleton<EntryService>();
-    builder.Services.AddSingleton<ContainerService>();
-
-    // Add Controller
-    builder.Services.AddSingleton<IAppController, AppControllerImpl>();
-    builder.Services.AddSingleton<IAppMatcherController, AppMatcherControllerImpl>();
-    builder.Services.AddSingleton<IImageController, ImageControllerImpl>();
-    builder.Services.AddSingleton<IEntryController, EntryControllerImpl>();
-
-    /**
-    * 由于应用需要集成到 Integration App 中发布
-    * 这里需要使用 AddApplicationPart 将 Controller 扫描到 builder 中
-    */
-    builder.Services.AddControllers(options =>
+    public static WebApplication CreateApplication()
     {
-      options.Filters.Add<HttpStandardExceptionFilter>();
-    });
+        var builder = WebApplication.CreateBuilder();
 
-    var app = builder.Build();
+        builder.Services.AddEndpointsApiExplorer();
 
-    // Database auto migrate
-    using (var scope = app.Services.CreateScope())
-    {
-      var db = scope.ServiceProvider.GetRequiredService<CanaRailsContext>();
-      db.Database.Migrate();
+        builder.Services.AddDbContext<CanaRailsContext>();
+
+        // Add Container adapter
+        builder.Services.AddSingleton<IAdapter, DockerAdapter>();
+
+        // Add Services
+        builder.Services.AddSingleton<AppService>();
+        builder.Services.AddSingleton<AppMatcherService>();
+        builder.Services.AddSingleton<DockerService>();
+        builder.Services.AddSingleton<ImageService>();
+        builder.Services.AddSingleton<EntryService>();
+        builder.Services.AddSingleton<ContainerService>();
+
+        // Add Controller
+        builder.Services.AddSingleton<IAppController, AppControllerImpl>();
+        builder.Services.AddSingleton<IAppMatcherController, AppMatcherControllerImpl>();
+        builder.Services.AddSingleton<IImageController, ImageControllerImpl>();
+        builder.Services.AddSingleton<IEntryController, EntryControllerImpl>();
+
+        builder.Services.AddControllers(options => { options.Filters.Add<HttpStandardExceptionFilter>(); });
+
+        var app = builder.Build();
+
+        app.MapControllers();
+
+        // 添加对内置 SPA 页面的支持
+        app.UseFileServer();
+        app.MapFallback(async (context) =>
+        {
+            context.Response.StatusCode = 200;
+            await context.Response.SendFileAsync(
+                Path.Join(app.Environment.WebRootPath, "index.html")
+            );
+        });
+
+        return app;
     }
 
-    app.MapControllers();
-
-    // 添加对内置 SPA 页面的支持
-    app.UseFileServer();
-    app.MapFallback(async (context) =>
+    public static Task Main()
     {
-      context.Response.StatusCode = 200;
-      await context.Response.SendFileAsync(
-        Path.Join(app.Environment.WebRootPath, "index.html")
-      );
-    });
-
-    return app;
-  }
-  public static Task Main()
-  {
-    return CreateApplication().RunAsync("http://localhost:8080");
-  }
+        return CreateApplication().RunAsync("http://localhost:8080");
+    }
 }
 
