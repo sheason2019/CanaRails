28f17d2dc3f48493192254f04ac964369945fa4c
sheason <811627877@qq.com>
2024-06-30 22:09:03 +0800

    feat: 添加流量入口和可用镜像计数

--- Dashboard.Web/src/pages/app/[appId]/components/app-stat-item.tsx
+++ Dashboard.Web/src/pages/app/[appId]/components/app-stat-item.tsx
@@ -1,4 +1,11 @@
-import { Card, CardBody, Stat, StatLabel, StatNumber } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  Skeleton,
+  Stat,
+  StatLabel,
+  StatNumber,
+} from "@chakra-ui/react";
 import { ReactNode } from "react";
 import { Link } from "react-router-dom";
 
@@ -6,16 +13,24 @@ interface Props {
   label: ReactNode;
   value: ReactNode;
   to: string;
+  isLoading: boolean;
 }
 
-export default function AppStatItem({ to, label, value }: Props) {
+export default function AppStatItem({ to, label, value, isLoading }: Props) {
   return (
     <Link to={to}>
       <Card _hover={{ shadow: "lg" }} className="transition-shadow">
         <CardBody>
           <Stat>
             <StatLabel>{label}</StatLabel>
-            <StatNumber>{value}</StatNumber>
+            <StatNumber mt={2}>
+              <Skeleton
+                width={isLoading ? 16 : undefined}
+                isLoaded={!isLoading}
+              >
+                {value}
+              </Skeleton>
+            </StatNumber>
           </Stat>
         </CardBody>
       </Card>
--- Dashboard.Web/src/pages/app/[appId]/components/app-stats.tsx
+++ Dashboard.Web/src/pages/app/[appId]/components/app-stats.tsx
@@ -1,9 +1,11 @@
 import { Heading } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 import AppStatItem from "./app-stat-item";
+import useAppStats from "../hooks/use-app-stats";
 
 export default function AppStats() {
   const { appId } = useParams();
+  const { data, isLoading } = useAppStats();
 
   return (
     <>
@@ -12,9 +14,24 @@ export default function AppStats() {
       </Heading>
       <p className="mb-3 text-sm text-gray-500">点击卡片查看数据详情</p>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <AppStatItem to={`/app/${appId}/entry`} label="流量入口" value="0" />
-        <AppStatItem to={`/app/${appId}/image`} label="可用镜像" value="0" />
-        <AppStatItem to={`/app/${appId}/log`} label="操作日志" value="0" />
+        <AppStatItem
+          to={`/app/${appId}/entry`}
+          label="流量入口"
+          value={data?.entry}
+          isLoading={isLoading}
+        />
+        <AppStatItem
+          to={`/app/${appId}/image`}
+          label="可用镜像"
+          value={data?.image}
+          isLoading={isLoading}
+        />
+        <AppStatItem
+          to={`/app/${appId}/log`}
+          label="操作日志"
+          value="0"
+          isLoading={isLoading}
+        />
       </div>
     </>
   );
